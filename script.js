00d8594d16bce11017c4b5d6f51fc5920ed01479
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-06-07 16:21:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
+let list=document.getElementById("todoList")
+function handleTodo()
+{
+	let todo=document.getElementById("newTodoInput")
+	if(todo.value!="")
+	{
+		let todoItem=document.createElement("li")
+		todoItem.innerText=todo.value
+		list.appendChild(todoItem)
+		todo.value=""
+	}
+	else{
+		alert("Empty submission of todo items is not allowed.")
+	}
+}
 
